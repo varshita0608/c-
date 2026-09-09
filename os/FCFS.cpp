2d2e6325ee8c38b500ca09c38eef1d6d2ed02745
Varshita Singh <varshita.25b01011381@abes.ac.in>
2026-09-09 09:34:14 +0530

    up

--- os/FCFS.cpp
+++ os/FCFS.cpp
@@ -1,28 +1,62 @@
 //first come first serve scheduling algorithm
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter the number of processes: ";
-    cin>>n;
-    int bt[n], wt[n], tat[n];
-    float awt=0, atat=0;
-    for(int i=0;i<n;i++){
-        cout<<"Enter the burst time of process: "<<i+1<<"";
-        cin>>bt[i];
-    }
-    wt[0]=0;
-    for(int i=1;i<n;i++){
-        wt[i]=wt[i-1]+bt[i-1];
-    }
-    for(int i=0;i<n;i++){
-        tat[i]=wt[i]+bt[i];
-    }
-    for(int i=0;i<n;i++){
-        awt+=wt[i];
-        atat+=tat[i];
-    }
-    cout<<"Average waiting time: "<<awt/n<<endl;
-    cout<<"Average turnaround time: "<<atat/n<<endl;
-    return 0;
-}   
+
+int main() {
+int n;
+
+cout << "Enter number of processes: ";
+cin >> n;
+
+int at[n], bt[n], ct[n], tat[n], wt[n];
+cout << "\nEnter Arrival Time and Burst Time:\n";
+
+for (int i = 0; i < n; i++) {
+cout << "P" << i + 1 << " Arrival Time: ";
+cin >> at[i];
+
+cout << "P" << i + 1 << " Burst Time: ";
+cin >> bt[i];
+}
+for (int i = 0; i < n - 1; i++) {
+for (int j = i + 1; j < n; j++) {
+if (at[i] > at[j]) {
+swap(at[i], at[j]);
+swap(bt[i], bt[j]);
+}
+}
+}
+ct[0] = at[0] + bt[0];
+
+for (int i = 1; i < n; i++) {
+if (ct[i - 1] < at[i])
+ct[i] = at[i] + bt[i];
+else
+ct[i] = ct[i - 1] + bt[i];
+}
+for (int i = 0; i < n; i++) {
+tat[i] = ct[i] - at[i];
+wt[i] = tat[i] - bt[i];
+}
+
+cout << "\nProcess\tAT\tBT\tCT\tTAT\tWT\n";
+
+float avgWT = 0, avgTAT = 0;
+
+for (int i = 0; i < n; i++) {
+cout << "P" << i + 1 << "\t"
+<< at[i] << "\t"
+<< bt[i] << "\t"
+<< ct[i] << "\t"
+<< tat[i] << "\t"
+<< wt[i] << endl;
+
+avgWT += wt[i];
+avgTAT += tat[i];
+}
+
+cout << "\nAverage Waiting Time = " << avgWT / n;
+cout << "\nAverage Turnaround Time = " << avgTAT / n;
+
+return 0;
+}
